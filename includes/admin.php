daa7da508a9ad5aacac5d80ea1a6830311cde9f0
Benjamin J. Balter <ben@balter.com>
2012-02-12 10:38:37 -0500

    3.2 compatability

--- includes/admin.php
+++ includes/admin.php
@@ -72,14 +72,53 @@ class Infinite_Scroll_Admin {
 	 & @uses send_to_editor() (javascript)
 	 */
 	function send_to_editor( $args ) {
+		global $wpdb;
 		
 		if ( $args['errors'] !== null )
 			return $args;
+			
+		if ( isset( $_GET['attachment_id'] ) ) {
+		
+			$id = $_GET['attachment_id'];
+			
+		//workaround for WP 3.2 non-flash upload
+		//not ideal, but works for an edge case
+		} else {
+		
+			//because we can't get the attachment ID at this point, try to pull it from the database
+			//look for the most recent parent-less attachment with same title and mime-type
+			$upload = $GLOBALS['HTTP_POST_FILES']['async-upload'];
+			$title = substr( $upload['name'], 0, strrpos( $upload['name'], '.' ) );
+			$id = $wpdb->get_var( "SELECT ID FROM $wpdb->posts WHERE post_type = 'attachment' AND post_mime_type = '" . $upload['type'] . "' AND post_parent = '0' AND post_title = '$title' ORDER BY ID DESC LIMIT 1" );
+			
+			//if for some reason we couldn't pull the ID, simply kick
+			//the user will just have to click insert to close the dialog
+			if ( !$id )
+				return $args;
+		
+		}
 		
 		//rely on WordPress's internal function to output script tags and call send_to_editor()
-		media_send_to_editor( wp_get_attachment_url( $_GET['attachment_id'] ) );
+		media_send_to_editor( wp_get_attachment_url( $id ) );
 		
 		return $args;
+		
+	}
+	
+	/**
+	 * Wrapper function to load the tinyMCE Editor
+	 * Used to allow fallback to pre-3.3 function
+	 * @param string $field the field to load the editor for
+	 */
+	function editor( $field ) {
+		
+		//3.3
+		if ( function_exists( 'wp_editor' ) )
+			wp_editor( $this->parent->options->loading[ $field ], "infinite_scroll[loading][{$field}]", array( 'media_buttons' => false, 'textarea_rows' => 5, 'teeny' => true ) );
+		
+		//3.2
+		else
+			the_editor( $this->parent->options->loading[ $field ], "infinite_scroll[loading][{$field}]", null, false );
 		
 	}
 
