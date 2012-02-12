90fc2c8be1b7b8a5a49ab138864ed6cdfc1ad07e
Benjamin J. Balter <ben@balter.com>
2012-02-12 11:54:55 -0500

    added legacy csv

---
+++ PresetDB.csv.php
@@ -0,0 +1,7 @@
+<?php /*Infinite-Scroll Preset DB. To use this in other programs, remove this and the bottom line.
+Version,111218
+inove,#main,"#main div.post",#pagenavi,"#pagenavi span.older a:first"
+"twenty ten",#content,div.navigation,"#content  div.post","div.navigation a:first"
+"twenty eleven",#content,#nav-below,"#content article.post","#nav-below .nav-previous a:first"
+atahualpa,td#middle,div.navigation-bottom,"td#middle div.post","div.newer a:link"
+End Preset DB*/ ?>
